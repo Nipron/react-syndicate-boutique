5c4bd83cf280d75427950f28bd15fa65ba136d3c
Vitalii Doroshenko <2328221@ukr.net>
2021-01-04 06:44:50 +0200

    end v2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './styles.scss';
-import Logo from '../../assets/syndboutique.png'
+import Logo from '../../assets/syndboutique.png';
+import {Link} from 'react-router-dom';
 
 const Header = props => {
     return (
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Syndicate Boutique Logo" />
+                    <Link to="/">
+                        <img src={Logo} alt="Syndicate Boutique Logo"/>
+                    </Link>
+                </div>
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>

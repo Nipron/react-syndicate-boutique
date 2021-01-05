7a8cbde97ca11f7f8057fea8e34ec85cbc5278db
Vitalii Doroshenko <2328221@ukr.net>
2021-01-05 03:37:45 +0200

    fff

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
     apiKey: "AIzaSyC6zu2LRgaRFk1Syrr9aUF-al0XLveOiF8",
-    authDomain: "react-syndicate-boutique.firebaseapp.com",
+    authDomain: "react-syndicate-boutique.netlify.app",
     projectId: "react-syndicate-boutique",
     storageBucket: "react-syndicate-boutique.appspot.com",
     messagingSenderId: "332340617168",
